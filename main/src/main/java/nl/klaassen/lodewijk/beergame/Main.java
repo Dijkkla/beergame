f2196a30fd17fe5aff1a6dde664cb7216cc3e827
Lodewijk Klaassen <la.klaassen@student.han.nl>
2025-07-24 17:12:33 +0200

    Update Main.java

--- main/src/main/java/nl/klaassen/lodewijk/beergame/Main.java
+++ main/src/main/java/nl/klaassen/lodewijk/beergame/Main.java
@@ -1,11 +1,14 @@
 package nl.klaassen.lodewijk.beergame;
 
 import nl.klaassen.lodewijk.beergame.gamedata.DistributionChain;
+import nl.klaassen.lodewijk.beergame.gamedata.identifiers.DistributorType;
 
 public class Main {
     public static void main(String[] args) {
-        DistributionChain distributionChain = new DistributionChain(3, 7);
+        DistributionChain distributionChain = new DistributionChain(2, 3);
 
-        System.out.println(distributionChain);
+//        System.out.println(distributionChain);
+
+        DistributionChain.Distributor distributor = distributionChain.getDistributor(DistributorType.WHOLESALER, 1);
     }
 }
